3de112ab2c28c48375e4e79e7c2aab39252b3c88
Yana Ionova <yana.timofeeva.94@mail.ru>
2021-02-10 23:13:19 +0300

    add new tasks in gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,13 @@
-const { src, dest, task, series } = require('gulp'); // dest-функция записывает результат
+const { src, dest, task, series, watch } = require('gulp'); // dest-функция записывает результат
 const rm = require('gulp-rm'); // удаляет папки
 const sass = require('gulp-sass'); // компилирует файлы scss в css
 const concat = require('gulp-concat'); //склеивает файлы в один
+const browserSync = require('browser-sync').create(); //открывать сайт в браузере
+const reload = browserSync.reload; // перезагружает стр сайта при изменениях
+const sassGlob = require('gulp-sass-glob'); //удобное подклбчение всезх файлов в main.scss
+const autoprefixer = require('gulp-autoprefixer'); // autoprefixer
+const px2rem = require('gulp-smile-px2rem'); // переводит px to rem
+const gcmq = require('gulp-group-css-media-queries'); // группирует одинаковые медиа запросы
 
 sass.compiler = require('node-sass'); //компилятор на node,js
 
@@ -9,8 +15,10 @@ task('clean', () => {
   return src('dist/**/*', { read: false }).pipe(rm());
 });
 
-task('copy', () => {
-  return src('src/scss/main.scss').pipe(dest('dist'));
+task('copy:html', () => {
+  return src('src/*.html')
+    .pipe(dest('dist'))
+    .pipe(reload({ stream: true }));
 });
 
 const styles = [
@@ -20,8 +28,29 @@ const styles = [
 task('styles', () => {
   return src(styles)
     .pipe(concat('main.scss'))
+    .pipe(sassGlob())
     .pipe(sass().on('error', sass.logError))
+    .pipe(px2rem())
+    .pipe(
+      autoprefixer({
+        browsers: ['last 2 versions'],
+        cascade: false,
+      })
+    )
+    .pipe(gcmq())
     .pipe(dest('dist'));
 });
 
-task('default', series('clean', 'styles'));
+task('server', () => {
+  browserSync.init({
+    server: {
+      baseDir: './dist',
+    },
+    open: false,
+  });
+});
+
+watch('./src/scss/**/*.scss', series('styles')); // следит за изменениями в scss
+watch('./src/*.html', series('copy:html')); // следит за изменениями в html
+
+task('default', series('clean', 'copy:html', 'styles', 'server'));
