4604a1315b4151cc4cd6c1fccf07ac15112a1751
Yana Ionova <yana.timofeeva.94@mail.ru>
2021-02-10 17:37:07 +0300

    modified gulpfile, add tasks

--- gulpfile.js
+++ gulpfile.js
@@ -1,12 +1,27 @@
-const { src, dest, task } = require('gulp'); // dest-функция записывает результат
+const { src, dest, task, series } = require('gulp'); // dest-функция записывает результат
 const rm = require('gulp-rm'); // удаляет папки
+const sass = require('gulp-sass'); // компилирует файлы scss в css
+const concat = require('gulp-concat'); //склеивает файлы в один
+
+sass.compiler = require('node-sass'); //компилятор на node,js
 
 task('clean', () => {
   return src('dist/**/*', { read: false }).pipe(rm());
 });
 
-function copy() {
+task('copy', () => {
   return src('src/scss/main.scss').pipe(dest('dist'));
-}
+});
 
-exports.copy = copy;
+const styles = [
+  'node_modules/normalize.css/normalize.css',
+  'src/scss/main.scss',
+];
+task('styles', () => {
+  return src(styles)
+    .pipe(concat('main.scss'))
+    .pipe(sass().on('error', sass.logError))
+    .pipe(dest('dist'));
+});
+
+task('default', series('clean', 'styles'));
