d50a2cf78d61dc85bc013b070cb306438f5f711a
Yana Ionova <yana.timofeeva.94@mail.ru>
2021-02-12 22:27:08 +0300

    create svg-sprites

--- gulpfile.js
+++ gulpfile.js
@@ -12,6 +12,8 @@ const cleanCSS = require('gulp-clean-css'); //сжимает файл
 const sourcemaps = require('gulp-sourcemaps'); //показывает на какой строчке расп интер.код
 const babel = require('gulp-babel'); // переводит новый синтаксис js в старый
 const uglify = require('gulp-uglify'); // сжимает js
+const svgo = require('gulp-svgo'); //в svg убирает fill, stroke и прочее
+const svgSprite = require('gulp-svg-sprite'); // создает спрайты
 
 sass.compiler = require('node-sass'); //компилятор на node,js
 
@@ -80,6 +82,56 @@ task('scripts', () => {
     .pipe(reload({ stream: true }));
 });
 
+task('icons:best', () => {
+  return src('src/img/icons/best/*.svg')
+    .pipe(
+      svgo({
+        plugins: [
+          {
+            removeAttrs: {
+              attrs: '(fill|stroke|style|width|height|data.*)',
+            },
+          },
+        ],
+      })
+    )
+    .pipe(
+      svgSprite({
+        mode: {
+          symbol: {
+            sprite: '../sprite.svg',
+          },
+        },
+      })
+    )
+    .pipe(dest('dist/img/icons/best'));
+});
+
+task('icons:footer', () => {
+  return src('src/img/icons/social/*.svg')
+    .pipe(
+      svgo({
+        plugins: [
+          {
+            removeAttrs: {
+              attrs: '(fill|stroke|style|width|height|data.*)',
+            },
+          },
+        ],
+      })
+    )
+    .pipe(
+      svgSprite({
+        mode: {
+          symbol: {
+            sprite: '../sprite.svg',
+          },
+        },
+      })
+    )
+    .pipe(dest('dist/img/icons/social'));
+});
+
 task('server', () => {
   browserSync.init({
     server: {
@@ -93,7 +145,9 @@ watch('./src/scss/**/*.scss', series('styles')); // следит за измен
 watch('./src/*.html', series('copy:html')); // следит за изменениями в html
 watch('./src/fonts/**/*', series('copy:fonts')); // следит за изменениями шрифтов
 watch('./src/img/**/*', series('copy:img')); // следит за изменениями в картинках
-watch('src/scripts/*.js*', series('scripts')); // следит за изменениями в js
+watch('src/scripts/*.js', series('scripts')); // следит за изменениями в js
+watch('src/icons/best/*.svg', series('icons:best')); // следит за изменениями в спрайтах
+watch('src/icons/social/*.svg', series('icons:footer')); // следит за изменениями в спрайтах
 
 task(
   'default',
@@ -104,6 +158,8 @@ task(
     'copy:img',
     'styles',
     'scripts',
+    'icons:best',
+    'icons:footer',
     'server'
   )
 );
