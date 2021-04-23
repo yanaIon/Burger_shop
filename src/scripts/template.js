e3e514b1fb2b9e7ce184153afc4c3aa6c6de31f7
Yana Ionova <yana.timofeeva.94@mail.ru>
2021-04-23 11:48:22 +0300

    add style in fullscrin menu

--- src/scripts/template.js
+++ src/scripts/template.js
@@ -7,6 +7,7 @@
   function handler() {
     // Этот метод добавляет элементу класс, указанный в скобках
     template.classList.add('fullscrin');
+    document.body.style.overflow = 'hidden';
   }
   //обработчик событий по клику
   menuLink.addEventListener('click', handler);
@@ -17,10 +18,8 @@
   function handler1() {
     //Этот метод удаляет из элемента класс, указанный в скобках
     template.classList.remove('fullscrin');
+    document.body.style.overflow = '';
   }
   //обработчик событий по клику на закрытие
   close.addEventListener('click', handler1);
 })();
-
-//повесить класс на боди оверфлов хидден когда открывается полноэкранное меню
-//когда закрываемБ отменить это
